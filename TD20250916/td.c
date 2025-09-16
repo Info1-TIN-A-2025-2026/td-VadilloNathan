d52bb447ec7233640b8fc14fd1b9955bb1b5534f
VadilloNathan <nathan.vadillo@heig-vd.ch>
2025-09-16 12:32:36 +0000

    added TD20250916

---
+++ TD20250916/td.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	unsigned int num_students = 23; // entier non singé, seulement +
+	int account_value = -42; // singed optionnel
+	float temperature = 18.1; // virgule flottante, 32 bit
+	double pi = 3.14158484864; // 64 bit
+}
