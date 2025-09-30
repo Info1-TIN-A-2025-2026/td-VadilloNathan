6e3a5c253f573626a6de8945b1f2a06cc3157a74
VadilloNathan <nathan.vadillo@heig-vd.ch>
2025-09-30 12:28:56 +0000

    added directory 20250930

---
+++ TD20250930/td.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	printf("Enter mass 1 [Tonnes]: ");
+	double Mass1 = 0;
+	scanf("%lf", &Mass1); 
+
+	printf("Enter mass 2 [Tonnes]: ");
+	double Mass2 = 0;
+	scanf("%lf", &Mass2); 
+
+	printf("Enter distance [Kilometer]: ");
+	double Distance = 0;
+	scanf("%lf", &Distance); 
+
+	if(Mass1 < 0 || Mass2 < 0 || Distance < 0)
+		{
+			puts("Error");
+		}
+		
+	else
+		{
+			const double G = 6.67e-11;
+
+			double Force = G * (Mass1/1000) * (Mass2/1000) / ((Distance/1000)*(Distance/1000));
+
+			printf("%.3g", Force);
+		}
+
+	
+
+}
